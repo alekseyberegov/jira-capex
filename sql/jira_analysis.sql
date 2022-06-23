37a1f23cab02b87b2450a466452be490a33b3cfe
clicktripz <l87dg5f1!>
2022-06-23 15:48:52 -0700

    Refactoring SQL

--- sql/jira_analysis.sql
+++ sql/jira_analysis.sql
@@ -1,38 +1,198 @@
+create table jira_issue_lifecycle (
+	issue_id				INTEGER NOT NULL,
+	issue_key				VARCHAR(250),
+	status_name				VARCHAR(250),
+	resolution_name			VARCHAR(250),
+	status_log				VARCHAR(4000),
+	issue_created			DATE,
+	issue_updated			DATE,
+	status_updated			DATE,
+	resolution_created		DATE,
+	issue_started			DATE,
+	issue_completed			DATE,
+	work_started			DATE,
+	work_ended				DATE,
+	PRIMARY KEY (issue_id)
+);
 	
 
-select count(1)
-from jira_changelog jc 
+select *
+	, round(cast(cnt as float) / cnt_total, 2) as sov
+	, round(cast(issue_cnt as float) / issue_total, 2) as issue_sov
+from (
+	select (items_len)
+		, count(1) cnt
+		, count(distinct issue_id) as issue_cnt
+		, sum(count(1)) over() as cnt_total
+		, sum(count(distinct issue_id)) over() as issue_total
+	from jira_changelog jc 
+	group by 1
+	order by 1
+)
+
+WITH RECURSIVE dates(x) AS ( 
+            SELECT '2015-01-01' 
+                UNION ALL 
+            SELECT DATE(x, '+1 MONTHS') FROM dates WHERE x<'2016-01-01' 
+        ) 
+        SELECT * FROM dates;
 
 
-alter table jira_changelog add column items_len INTEGER;
-alter table jira_changelog add column snapshot_date DATE;
+select * from jira_changelog 
 
+
+
+select count(1) from jira_issues ji 
+
+select status_name_from, status_name_to, count(1)
+from jira_status js 
+group by 1, 2
+order by 3 desc
+
+
+select count(1), count(distinct issue_id)
+from jira_status js 
+
+select COALESCE(workflow, status_name) as wf
+	, sum(case when workflow like '%In Progress%' then 1 else 0 end) as in_progress
+	, count(1) as cnt
+	, sum(count(1)) over() as total
+from (
+	select id,  ji.created_date, ji.status_name, ji.updated_date, ji.status_change_date , workflow
+	from jira_issues ji left join (
+		select issue_id, group_concat(status_name_from||'->'||status_name_to , ',') as workflow
+		from jira_status js 
+		group by 1
+	) s on (s.issue_id = ji.id)
+)
+--where workflow not like '%In Progres%'
+group by 1
+order by 3 desc
+
+	, sum((JULIANDAY(status_updated) - JULIANDAY(COALESCE(prev_status_updated, issue_created))) * from_progress) AS work_days
+	
+	
+delete from jira_issue_lifecycle 
+
+insert into jira_issue_lifecycle(
+	  issue_id
+	, issue_key
+	, status_name
+	, resolution_name
+	, status_log
+	, issue_created
+	, issue_updated
+	, status_updated
+	, resolution_created
+	, issue_started
+	, issue_completed
+	, work_started
+	, work_ended)	
+select issue_id
+	, issue_key
+	, status_name
+	, resolution_name
+	, '['||group_concat('{"s0":"'||s0||'","t0":"'||t0||'","s1":"'||s1||'","t1":"'||t1||'"}', ',')||']' as status_log
+	, min(issue_created) as issue_created
+	, max(issue_updated) as issue_updated
+	, max(status_updated) as status_updated
+	, max(resolution_date) as resolution_created
+	, min(case when s0 in ('Scheduled', 'Open', 'Backlog', 'To Do') then t1 end) as issue_started
+	, min(case when s1 in ('Done', 'Released', 'Accepted', 'Closed', 'Finished', 'Old Done') then t1 end) as issue_completed
+	, min(case when from_progress = 1 then t0 end) as work_started
+	, max(case when from_progress = 1 then t1 end) as work_ended
+from (
+	select issue_id
+		, js.created_date as t1
+		, IFNULL(lag(js.created_date) 
+			over(partition by issue_id order by js.issue_id, js.created_date), ji.created_date) as t0
+		, ji.created_date as issue_created
+		, ji.updated_date as issue_updated
+		, ji.status_change_date as status_updated
+		, ji."key" as issue_key
+		, ji.status_name 
+		, ji.resolution_name 
+		, ji.resolution_date 
+		, case when status_name_from = 'In Progress' then 1 else 0 end as from_progress
+		, js.status_name_from as s0
+		, js.status_name_to   as s1
+	from jira_status js inner join jira_issues ji on (js.issue_id = ji.id)
+	where status_name not in ('Won''t Do') 
+		and resolution_name not in ('Won''t Do', 'Duplicate')
+	order by 1, 2
+) d
+group by 1, 2
+
+
+delete from jira_issue_lifecycle
+
+select JULIANDAY(end_date) - JULIANDAY(start_date) + 1 as duration, *
+from (
+	select COALESCE(work_started, issue_started, issue_created) as start_date
+		,  COALESCE(work_ended, issue_completed) as end_date
+		, ji.*
+	from jira_issue_lifecycle  ji
+)
+order by 1 desc
+
+select status_name_to, 1, count(1)
+from jira_status js 
+group by 1, 2
+
+
+
+select '{ ''status'': "'||ifnull(status_name, 'None')||
+	'", ''resolution_name'': "'||ifnull(resolution_name, 'None')||
+	'", ''resolution_description'': "'||ifnull(resolution_description, 'None')||'"},' as json, count(1) as cnt
+from jira_issues ji 
+group by 1
+order by 2 desc
 
 INSERT into jira_status(id, status_name_from, status_from, status_name_to, status_to, issue_id, created_date , author_id, author_name )
 		select id
-					, case when items_0_field_name = 'status' then items_0_from_text
+				 , case when items_0_field_name = 'status' then items_0_from_text
 						when items_1_field_name = 'status' then items_1_from_text
 						when items_2_field_name = 'status' then items_2_from_text
 						when items_3_field_name = 'status' then items_3_from_text
-						when items_4_field_name = 'status' then items_4_from_text			
+						when items_4_field_name = 'status' then items_4_from_text	
+						when items_5_field_name = 'status' then items_5_from_text
+						when items_6_field_name = 'status' then items_6_from_text
+						when items_7_field_name = 'status' then items_7_from_text
+						when items_8_field_name = 'status' then items_8_from_text			
+						when items_9_field_name = 'status' then items_9_from_text					
 					end as status_name_from
-					, case when items_0_field_name = 'status' then items_0_from
+				 , case when items_0_field_name = 'status' then items_0_from
 						when items_1_field_name = 'status' then items_1_from
 						when items_2_field_name = 'status' then items_2_from
 						when items_3_field_name = 'status' then items_3_from
-						when items_4_field_name = 'status' then items_4_from		
+						when items_4_field_name = 'status' then items_4_from	
+						when items_5_field_name = 'status' then items_5_from		
+						when items_6_field_name = 'status' then items_6_from		
+						when items_7_field_name = 'status' then items_7_from		
+						when items_8_field_name = 'status' then items_8_from
+						when items_9_field_name = 'status' then items_9_from			
 					end as status_from
-					, case when items_0_field_name = 'status' then items_0_to_text
+				 , case when items_0_field_name = 'status' then items_0_to_text
 						when items_1_field_name = 'status' then items_1_to_text
 						when items_2_field_name = 'status' then items_2_to_text
 						when items_3_field_name = 'status' then items_3_to_text
-						when items_4_field_name = 'status' then items_4_to_text			
+						when items_4_field_name = 'status' then items_4_to_text		
+						when items_5_field_name = 'status' then items_5_to_text	
+						when items_6_field_name = 'status' then items_6_to_text	
+						when items_7_field_name = 'status' then items_7_to_text	
+						when items_8_field_name = 'status' then items_8_to_text	
+						when items_9_field_name = 'status' then items_9_to_text		
 					end as status_name_to
-					, case when items_0_field_name = 'status' then items_0_to
+				 , case when items_0_field_name = 'status' then items_0_to
 						when items_1_field_name = 'status' then items_1_to
 						when items_2_field_name = 'status' then items_2_to
 						when items_3_field_name = 'status' then items_3_to
 						when items_4_field_name = 'status' then items_4_to	
+						when items_5_field_name = 'status' then items_5_to	
+						when items_6_field_name = 'status' then items_6_to	
+						when items_7_field_name = 'status' then items_7_to	
+						when items_8_field_name = 'status' then items_8_to	
+						when items_9_field_name = 'status' then items_9_to	
 					end as status_to
 				, issue_id
 				, created_date 
@@ -40,11 +200,16 @@ INSERT into jira_status(id, status_name_from, status_from, status_name_to, statu
 				, author_name
 			from jira_changelog 
 			where items_0_field_name = 'status'
-				or items_1_field_name = 'status'
-					or items_2_field_name = 'status'
-						or items_3_field_name = 'status'
-							or items_4_field_name = 'status'
-
+					or items_1_field_name = 'status'
+						or items_2_field_name = 'status'
+							or items_3_field_name = 'status'
+								or items_4_field_name = 'status'
+									or items_5_field_name = 'status'
+										or items_6_field_name = 'status'
+											or items_7_field_name = 'status'
+												or items_8_field_name = 'status'
+													or items_9_field_name = 'status'
+							
 
 select 'map_'||lower(project_key), count(1), max(created_date)
 from jira_issues ji 
--- sql/jira_migrate.sql
+++ sql/jira_migrate.sql
@@ -5,25 +5,45 @@ INSERT into jira_status(id, status_name_from, status_from, status_name_to, statu
 						when items_1_field_name = 'status' then items_1_from_text
 						when items_2_field_name = 'status' then items_2_from_text
 						when items_3_field_name = 'status' then items_3_from_text
-						when items_4_field_name = 'status' then items_4_from_text			
+						when items_4_field_name = 'status' then items_4_from_text	
+						when items_5_field_name = 'status' then items_5_from_text
+						when items_6_field_name = 'status' then items_6_from_text
+						when items_7_field_name = 'status' then items_7_from_text
+						when items_8_field_name = 'status' then items_8_from_text			
+						when items_9_field_name = 'status' then items_9_from_text					
 					end as status_name_from
 				 , case when items_0_field_name = 'status' then items_0_from
 						when items_1_field_name = 'status' then items_1_from
 						when items_2_field_name = 'status' then items_2_from
 						when items_3_field_name = 'status' then items_3_from
-						when items_4_field_name = 'status' then items_4_from		
+						when items_4_field_name = 'status' then items_4_from	
+						when items_5_field_name = 'status' then items_5_from		
+						when items_6_field_name = 'status' then items_6_from		
+						when items_7_field_name = 'status' then items_7_from		
+						when items_8_field_name = 'status' then items_8_from
+						when items_9_field_name = 'status' then items_9_from			
 					end as status_from
 				 , case when items_0_field_name = 'status' then items_0_to_text
 						when items_1_field_name = 'status' then items_1_to_text
 						when items_2_field_name = 'status' then items_2_to_text
 						when items_3_field_name = 'status' then items_3_to_text
-						when items_4_field_name = 'status' then items_4_to_text			
+						when items_4_field_name = 'status' then items_4_to_text		
+						when items_5_field_name = 'status' then items_5_to_text	
+						when items_6_field_name = 'status' then items_6_to_text	
+						when items_7_field_name = 'status' then items_7_to_text	
+						when items_8_field_name = 'status' then items_8_to_text	
+						when items_9_field_name = 'status' then items_9_to_text		
 					end as status_name_to
 				 , case when items_0_field_name = 'status' then items_0_to
 						when items_1_field_name = 'status' then items_1_to
 						when items_2_field_name = 'status' then items_2_to
 						when items_3_field_name = 'status' then items_3_to
 						when items_4_field_name = 'status' then items_4_to	
+						when items_5_field_name = 'status' then items_5_to	
+						when items_6_field_name = 'status' then items_6_to	
+						when items_7_field_name = 'status' then items_7_to	
+						when items_8_field_name = 'status' then items_8_to	
+						when items_9_field_name = 'status' then items_9_to	
 					end as status_to
 				, issue_id
 				, created_date 
@@ -31,30 +51,62 @@ INSERT into jira_status(id, status_name_from, status_from, status_name_to, statu
 				, author_name
 			from jira_changelog 
 			where items_0_field_name = 'status'
-				or items_1_field_name = 'status'
-					or items_2_field_name = 'status'
-						or items_3_field_name = 'status'
-							or items_4_field_name = 'status'
+					or items_1_field_name = 'status'
+						or items_2_field_name = 'status'
+							or items_3_field_name = 'status'
+								or items_4_field_name = 'status'
+									or items_5_field_name = 'status'
+										or items_6_field_name = 'status'
+											or items_7_field_name = 'status'
+												or items_8_field_name = 'status'
+													or items_9_field_name = 'status'
 							
-							
-							
 
-alter table jira_ol add column capex_category varchar(250);
-alter table jira_ol add column project_driver varchar(250);
-alter table jira_ol add column assignee_id VARCHAR(250);
-alter table jira_ol add column assignee_email VARCHAR(250);
-alter table jira_ol add column assignee_name VARCHAR(250);
-alter table jira_ol add column assignee_timezone VARCHAR(250);
-alter table jira_ol add column creator_id VARCHAR(250);
-alter table jira_ol add column resolution_date DATE;
-alter table jira_ol add column resolution_name VARCHAR(250);
-alter table jira_ol add column resolution_description VARCHAR(250);
-
-
-alter table jira_ol add column snapshot_date DATE;
-alter table jira_ol add column content_len INTEGER;
-
-alter table jira_issues add column snapshot_date DATE;
-
-alter table jira_changelog add column items_len INTEGER;
-alter table jira_changelog add column snapshot_date DATE;
+insert into jira_issue_lifecycle(
+	  issue_id
+	, issue_key
+	, status_name
+	, resolution_name
+	, status_log
+	, issue_created
+	, issue_updated
+	, status_updated
+	, resolution_created
+	, issue_started
+	, issue_completed
+	, work_started
+	, work_ended)	
+select issue_id
+	, issue_key
+	, status_name
+	, resolution_name
+	, '['||group_concat('{"s0":"'||s0||'","t0":"'||t0||'","s1":"'||s1||'","t1":"'||t1||'"}', ',')||']' as status_log
+	, min(issue_created) as issue_created
+	, max(issue_updated) as issue_updated
+	, max(status_updated) as status_updated
+	, max(resolution_date) as resolution_created
+	, min(case when s0 in ('Scheduled', 'Open', 'Backlog', 'To Do') then t1 end) as issue_started
+	, min(case when s1 in ('Done', 'Released', 'Accepted', 'Closed', 'Finished', 'Old Done') then t1 end) as issue_completed
+	, min(case when from_progress = 1 then t0 end) as work_started
+	, max(case when from_progress = 1 then t1 end) as work_ended
+from (
+	select issue_id
+		, js.created_date as t1
+		, IFNULL(lag(js.created_date) 
+			over(partition by issue_id order by js.issue_id, js.created_date), ji.created_date) as t0
+		, ji.created_date as issue_created
+		, ji.updated_date as issue_updated
+		, ji.status_change_date as status_updated
+		, ji."key" as issue_key
+		, ji.status_name 
+		, ji.resolution_name 
+		, ji.resolution_date 
+		, case when status_name_from = 'In Progress' then 1 else 0 end as from_progress
+		, js.status_name_from as s0
+		, js.status_name_to   as s1
+	from jira_status js inner join jira_issues ji on (js.issue_id = ji.id)
+	where status_name not in ('Won''t Do') 
+		and resolution_name not in ('Won''t Do', 'Duplicate')
+	order by 1, 2
+) d
+group by 1, 2
